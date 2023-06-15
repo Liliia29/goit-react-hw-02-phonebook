151c29a0e1f7b76365a5be66b8a4c551e2388f0e
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-06-15 14:03:16 +0300

    react-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { Container } from './App.styled';
+// import { Section } from 'components/Section';
+// import { FeedbackOptions } from 'components/FeedbackOptions';
+// import { Statistics } from 'components/Statistics';
+// import { Notification } from 'components/Notification';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+    name: '',
+    number: ''
+  }
+
+  // onLeaveFeedback = state => {
+  //   this.setState(prevState => ({ [state]: prevState[state] + 1 }));
+  // };
+
+  // countTotalFeedback = () => {
+  //   const { good, neutral, bad } = this.state;
+  //   return good + neutral + bad;
+  // };
+
+  // countPositiveFeedback = () => {
+  //   const { good } = this.state;
+  //   const total = this.countTotalFeedback();
+  //   return Math.round((good / total) * 100) || 0;
+  // };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const feedbackOptions = Object.keys(this.state);
+//     const total = this.countTotalFeedback();
+//     const positivePercentage = this.countPositiveFeedback();
+
+//     return (
+//       <Container>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             onLeaveFeedback={this.onLeaveFeedback}
+//             feedbackOptions={feedbackOptions}
+//           />
+//         </Section>
+
+//         <Section title="Statistics">
+//           {total ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={positivePercentage}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </Container>
+//     );
+//   }
+}
---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    row-gap: 10px;
+`;
+
+export const ListItem = styled.li`
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+    color: white;
+`;
+
+export const Button = styled.button`
+    min-width: 90px;
+    padding: 6px 12px;
+    
+    font-family: inherit;
+    font-size: 16px;
+
+    margin: 0;
+    margin-left: auto;
+
+    background-color: white;
+    border: 2px solid black;
+    border-radius: 4px;
+`;
