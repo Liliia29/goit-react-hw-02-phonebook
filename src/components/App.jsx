114b547fe7a61ce07a5cdce66e78fd0282f78977
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-06-20 13:25:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,7 @@ export class App extends Component {
   };
 
   handleFormSubmit = contact => {
-    const isInContacts = this.state.contactssome(
+    const isInContacts = this.state.contacts.some(
       ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
     );
     if (isInContacts) {
